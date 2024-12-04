0971e779587867591167f765c52765542042c7d3
AJ for the Sky <no@no.no>
2024-12-04 20:12:23 +0100

    config

---
+++ config.py
@@ -0,0 +1,14 @@
+TOKEN: str = 'MTE3NTQ0MzU2MzQxNzQ0NDQxMw.GeVpN9.tXc2X_WOTKG7e4qbAhlFgJJfmd6YVBihZxFHBU'
+
+DEFAULT_GUILD: int = 810552703155437568
+
+BOT_OWNER: int = 729457100580978718
+COMMUNITY_CHAT: int = 1084012060163190836
+
+ALLOWED_CHANNELS: list[int] = [810688183486119966, 842787358596005998, 810664811557290025, 810567071158501416]
+
+
+EVENT_DESCR: str = ('Jeden Tag wird zu einer zufälligen Uhrzeit ein Türchen im Chat spawnen und der erste, der dieses Türchen aufmacht,'
+                    'bekommt einen Punkt. Am 24.12 wird der Gewinner entschieden und dieser bekommt einen kleinen Preis.')
+
+
